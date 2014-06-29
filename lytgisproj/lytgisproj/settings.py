f848621942eadc212b8f687373d21ed78efb4878
berserk <berserk@hellsing2.(none)>
2014-06-29 19:11:41 +0800

    Added wsgi support

--- lytgisproj/lytgisproj/settings.py
+++ lytgisproj/lytgisproj/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wsgi',
     'south',
+    'lytgis',
 )
 
 MIDDLEWARE_CLASSES = (
--- lytgisproj/lytgisproj/wsgi.py
+++ lytgisproj/lytgisproj/wsgi.py
@@ -1,14 +1,19 @@
-"""
-WSGI config for lytgisproj project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lytgisproj.settings")
+import sys
+import site
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/berserk/.envs/lytgisproj/local/lib/python2.7/site-packages/')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/berserk/projects/lytgisproj/')
+sys.path.append('/home/berserk/projects/lytgisproj/lytgisproj/')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'lytgisproj.settings'
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/home/berserk/.envs/lytgisproj/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
